890dd4a33cb6f467fa3058e7ea3aaafca24d6dfc
Juliet Sabol <julietsabol@gmail.com>
2018-03-07 22:21:12 +0700

    static serve files fixes

--- server/server.js
+++ server/server.js
@@ -6,11 +6,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 const path = require('path');
 
-process.env.PWD = process.cwd();
-
 if (process.env.NODE_ENV === 'production') {
-	const staticPath = express.static(path.join(process.env.PWD, '/../client/build');
-	app.use('/', staticPath);
+	const staticPath = path.join(__dirname, '/../client/build');
+	app.use(express.static(staticPath));
 	console.log("Serving static files at / from "+staticPath);
 }
 
