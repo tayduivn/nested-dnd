a184b6c73313b202f55dca7bea8ad7d0973a0f0f
Juliet Sabol <julietsabol@gmail.com>
2018-03-07 21:59:25 +0700

    removed __dirname for heroku compatibility

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3001;
+const path = require('path');
+
+process.env.PWD = process.cwd();
 
 if (process.env.NODE_ENV === 'production') {
-	app.use('/', express.static(`${__dirname}/../client/build`));
+	app.use('/', express.static(path.join(process.env.PWD, '/../client/build')));
 }
 
 app.get("/api/hello", (req, res) => {
