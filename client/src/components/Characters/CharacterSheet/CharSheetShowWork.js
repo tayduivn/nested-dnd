428ed6f4771302296f8eeac3f910dbf6fcf0284b
Juliet Sabol <julietsabol@gmail.com>
2018-03-27 23:53:21 +0700

    Reduce duplication

--- client/src/components/Characters/CharacterSheet/CharSheetShowWork.js
+++ client/src/components/Characters/CharacterSheet/CharSheetShowWork.js
@@ -3,87 +3,29 @@ import PropTypes from "prop-types";
 
 import { Background } from "../../../stores/CharRolePlay"
 
-export default class ShowWork extends Component {
-	static get propTypes() {
-		return {
-			background: PropTypes.instanceOf(Background),
-			classes: PropTypes.array,
-			col: PropTypes.number
-		};
-	}
-	subclasses(classes) {
-		return classes.map(c => {
-			var subclasses = [];
-			for (var name in c.subclasses) {
-				var list = c.subclasses[name];
-				if (list.join) list = list.join(", ");
 
-				subclasses.push(
-					<span key={name}>
-						{name}: {list}
-						<br />
-					</span>
-				);
-			}
-			return subclasses;
-		});
+function makeList(map){
+	var arr = [];
+	for (var name in map) {
+		var list = map[name];
+		if (list.join) list = list.join(", ");
+		arr.push(<ListItem name={name} list={list} />);
 	}
-	render() {
-		var subclasses = this.subclasses(this.props.classes);
-		return (
-			<div className={`close-col col-${this.props.col}`}>
-				<p className="title-sm">Personality Trait</p>
-				<p>
-					{this.props.background.personality}
-				</p>
-				<p className="title-sm">Ideal</p>
-				<p>
-					{this.props.background.ideal}
-				</p>
-				<p className="title-sm">Bond</p>
-				<p>
-					{this.props.background.bond}
-				</p>
-				<p className="title-sm">Flaw</p>
-				<p>
-					{this.props.background.flaw}
-				</p>
-				<Item
-					className={subclasses.length ? "" : "hidden"}
-					label="Subclasses"
-					value={this.props.classes.map((c,i) =>
-						<Subclass subclasses={c.subclasses} key={i} />
-					)}
-				/>
-			</div>
-		);
-	}
+	return arr;
 }
 
-class Subclass extends Component {
-	static get propTypes() {
-		return {
-			subclasses: PropTypes.object
-		};
-	}
-	render() {
-		var jsx = [];
-		for (var name in this.props.subclasses) {
-			var list = this.props.subclasses[name];
-			if (list.join) list = list.join(", ");
+const Subclass = ({subclasses}) => (
+	<p>{makeList(subclasses)}</p>
+)
 
-			jsx.push(
-				<span key={name}>
-					{name}: {list}
-					<br />
-				</span>
-			);
-		}
-		return <p>{jsx}</p>;
-	}
-}
+const ListItem = ({name, list}) =>(
+	<span key={name}>
+		{name}: {list}
+		<br />
+	</span>
+)
 
-export const Item = ({ xs, label, value}) => (
+const Item = ({ xs, label, value}) => (
 	<div className={"col-"+(xs ? xs : 4)}>
 		<p className="title-sm">
 			{label}
@@ -93,3 +35,34 @@ export const Item = ({ xs, label, value}) => (
 		</div>
 	</div>
 );
+
+const ShowWork = ({col, background, classes}) => (
+	<div className={`close-col col-${col}`}>
+		<p className="title-sm">Personality Trait</p>
+		<p>
+			{background.personality}
+		</p>
+		<p className="title-sm">Ideal</p>
+		<p>
+			{background.ideal}
+		</p>
+		<p className="title-sm">Bond</p>
+		<p>
+			{background.bond}
+		</p>
+		<p className="title-sm">Flaw</p>
+		<p>
+			{background.flaw}
+		</p>
+		<Item
+			className={classes.map(c => makeList(c.subclasses)).length ? "" : "hidden"}
+			label="Subclasses"
+			value={classes.map((c,i) =>
+				<Subclass subclasses={c.subclasses} key={i} />
+			)}
+		/>
+	</div>
+);
+
+export default ShowWork;
+export { Item };
