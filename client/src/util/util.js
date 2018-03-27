b3485d718d09f6d3ced3ed71f3262823b2a86dce
Juliet Sabol <julietsabol@gmail.com>
2018-03-27 23:31:54 +0700

    Reduce return statements

--- client/src/util/util.js
+++ client/src/util/util.js
@@ -2,24 +2,17 @@
 
 Array.prototype.equals = function (array) { // eslint-disable-line
 		// if the other array is a falsy value, return
-		if (!array)
-				return false;
-
-		// compare lengths - can save a lot of time 
-		if (this.length !== array.length)
+		if (!array || this.length !== array.length)
 				return false;
 
 		for (var i = 0, l=this.length; i < l; i++) {
 				// Check if we have nested arrays
 				if (this[i] instanceof Array && array[i] instanceof Array) {
 						// recurse into the nested arrays
-						if (!this[i].equals(array[i]))
-								return false;       
-				}           
-				else if (this[i] !== array[i]) { 
-						// Warning - two different object instances will never be equal: {x:20} != {x:20}
-						return false;   
-				}           
+						if (!this[i].equals(array[i])) return false;       
+				} 
+				// Warning - two different object instances will never be equal: {x:20} != {x:20}        
+				else if (this[i] !== array[i]) return false;        
 		}       
 		return true;
 }
